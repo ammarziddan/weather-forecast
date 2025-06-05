2a023b51a8959141ddb7aee40bac906f932ab413
Ammar Ziddan <ammarziddan@gmail.com>
2025-06-06 02:51:19 +0700

    set geolocation api, minor UI update, simple error handling

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-const weatherAPI = 'https://api.weatherapi.com/v1/forecast.json?key=14afd0c9ee4c4780868134847250306&q=auto:ip&days=1';
+let baseURL = 'https://api.weatherapi.com/v1/forecast.json?key=14afd0c9ee4c4780868134847250306&days=1';
+const defaultLocation = '&q=auto:ip';
+let weatherAPI = baseURL + defaultLocation;
+
+// GET USER LOCATION
+function getPos() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+}
+
+async function updatePosition() {
+    try {
+        let pos = await getPos();
+        if(pos.code === 1) {
+            throw new Error(pos)
+        }
+        weatherAPI = baseURL + `&q=${pos.coords.latitude},${pos.coords.longitude}`;
+        // update ui with fixed position
+        updateUI(weatherAPI);
+    } catch (error) {
+        // nothing to do when user rejected locataion
+    }
+}
+// ---GET USER LOCATION
+
+// UPDATE UI
+// set placeholder UI
+function setPlaceholderUI() {
+    let forecastCards = ''
+    const card = /*html*/`<div class="forecast-element d-flex flex-column text-center rounded-4 p-2 fw-light placeholder-glow">
+                              <p><span class="placeholder h-25 w-50"></span></p>
+                              <div class="placeholder-forecast placeholder"></div>
+                              <p><span class="placeholder h-25 w-50"></span></p>
+                          </div>`;
+    // add placeholder card 24 times
+    for(let i = 0; i < 24; i++) {
+        forecastCards += card;
+    }
+    const forecastWrapper = document.querySelector('#forecast-wrapper');
+    forecastWrapper.innerHTML = forecastCards;
+}
 
 function updateCurrentData(data) {
     return /*html*/ `<div class="col-md-5 d-flex flex-column justify-content-start mb-3 ps-5">
@@ -7,7 +48,7 @@ function updateCurrentData(data) {
                         <h4 class="fs-4 fw-normal mb-2">Timezone : ${data.location.tz_id}</h4>
                         <h5 class="fw-light fs-6">Last Updated: ${data.current.last_updated}</h5>
                     </div>
-                    <div class="col-md-5 ps-5 ps-md-0">
+                    <div class="col-md-5 ps-5 ps-lg-0">
                         <h2 class="fs-2 fw-medium">${data.current.condition.text}</h2>
                         <div class="row justify-content-start gap-md-3">
                             <div class="col-lg-2 col-3">
@@ -54,26 +95,43 @@ function updateForecastData(data, currentHour) {
     }
 }
 
+const updateUI = async function(url) {
+    const currentData = await fetchWeather(url);
+    try {
+        if(currentData.error) {
+            throw currentData.error;
+        }
+        // update current ui
+        const currentUI = document.querySelector('#current-wrapper');
+        currentUI.innerHTML = updateCurrentData(currentData);
+        // update forecast ui
+        const forecastData = currentData.forecast.forecastday[0].hour;
+        const currentHour = currentData.location.localtime.split(" ")[1].split(":")[0];
+        let forecastCards = ''
+        forecastData.forEach(data => {
+            forecastCards += updateForecastData(data, currentHour);
+        });
+        const forecastWrapper = document.querySelector('#forecast-wrapper');
+        forecastWrapper.innerHTML = forecastCards;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+// ---UPDATE UI
+
+// FETCH WEATHER API
 function fetchWeather(url) {
     return fetch(url)
         .then(response => response.json())
         .then(data => data);
 }
+// ---FETCH WEATHER API
 
-const updateUI = async function() {
-    const currentData = await fetchWeather(weatherAPI);
-    // update current ui
-    const currentUI = document.querySelector('#current-wrapper');
-    currentUI.innerHTML = updateCurrentData(currentData);
-    // update forecast ui
-    const forecastData = currentData.forecast.forecastday[0].hour;
-    const currentHour = currentData.location.localtime.split(" ")[1].split(":")[0];
-    let forecastCards = ''
-    forecastData.forEach(data => {
-        forecastCards += updateForecastData(data, currentHour);
-    });
-    const forecastWrapper = document.querySelector('#forecast-wrapper');
-    forecastWrapper.innerHTML = forecastCards;
+// RUN FUNCTIONS
+setPlaceholderUI();
+
+updateUI(weatherAPI);
+
+if ("geolocation" in navigator) {
+    updatePosition();
 }
-
-updateUI();
